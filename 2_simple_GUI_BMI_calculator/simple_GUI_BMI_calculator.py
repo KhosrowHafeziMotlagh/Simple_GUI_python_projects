7172d8562bc4e2500877213e7808b8c0c3f9a630
KHOSROW <18746175+KhosrowHafeziMotlagh@users.noreply.github.com>
2025-08-25 03:26:31 +0330

    Update simple_GUI_BMI_calculator.py

--- 2_simple_GUI_BMI_calculator/simple_GUI_BMI_calculator.py
+++ 2_simple_GUI_BMI_calculator/simple_GUI_BMI_calculator.py
@@ -8,32 +8,32 @@ Training for : tkinter GUI , functions , lambda
 """
 
 
-from tkinter import *
+from tkinter import *                                                                                                   #وارد کردن کتابخانه گرافیکی tkinter
 
-font_size = 15
+font_size = 15                                                                                                          #تعریف یه متغیر عمومی برای فونت (از همین جا که تغییر بدیم، هر جا استفتده شده، اعمال میشه)
 def calculate(weight_inp, heghit_inp):
-    bmi = float(weight_inp.get()) / (float(heghit_inp.get()) ** 2)
-    result_label.config(text="BMI : " + str(bmi))
+    #تابع محسابه BMI
+    bmi = float(weight_inp.get()) / (float(heghit_inp.get()) ** 2)                                                      #شاخص توده بدنی قد و وزن گرفته شده رو رو حساب می کنیم
+    result_label.config(text="BMI : " + str(bmi))                                                                       #و نتیجه رو در لیبل نمایش میدیم
 
 
-main_form = Tk()
-weight_inp = StringVar()
-heghit_inp = StringVar()
+main_form = Tk()                                                                                                        #ایجاد فرم
+weight_inp = StringVar()                                                                                                #تعریف ورودی وزن
+heghit_inp = StringVar()                                                                                                #تعریف ورودی قد
 
-main_form.title("simple GUI BMI calculator by Khosrow Hafezi Motlagh")
+main_form.title("simple GUI BMI calculator by Khosrow Hafezi Motlagh")                                                  #نام گذاری فرم(بالا سمت راست فرم)
 main_form.config(bg="silver")
 
-weight_label = Label(text="Weight (kg):", font=("arial", font_size), width=11).grid(row=0, column=0)
-weight_entry = Entry(textvariable=weight_inp, bd=6, font=("arial", font_size),width=30).grid(row=0, column=1)
+weight_label = Label(text="Weight (kg):", font=("arial", font_size), width=11).grid(row=0, column=0)                    #ساخت لیبل وزن
+weight_entry = Entry(textvariable=weight_inp, bd=6, font=("arial", font_size),width=30).grid(row=0, column=1)           #ساخت اینتری وزن
 
-heghit_label = Label(text="Height (m):", font=("arial", font_size), width=11).grid(row=1, column=0)
-heghit_entry = Entry(textvariable=heghit_inp, bd=6, font=("arial", font_size),width=30).grid(row=1, column=1)
+heghit_label = Label(text="Height (m):", font=("arial", font_size), width=11).grid(row=1, column=0)                     #ساخت لیبل قد
+heghit_entry = Entry(textvariable=heghit_inp, bd=6, font=("arial", font_size),width=30).grid(row=1, column=1)           #ساخت اینتری قد
 
-result_label = Label(text="Bmi : ", bg="yellow", font=("arial", font_size))
+result_label = Label(text="Bmi : ", bg="yellow", font=("arial", font_size))                                             #ساخت لیبل نتیجه
 
-calculate_btn = Button(text="Calculate:", font=("arial", font_size),
-                       command=lambda: calculate(weight_inp, heghit_inp),bg="green",fg = "white", bd=6).grid(row=3, column=0, columnspan=2)
+calculate_btn = Button(text="Calculate:", font=("arial", font_size),command=lambda: calculate(weight_inp, heghit_inp),bg="green",fg = "white", bd=6).grid(row=3, column=0, columnspan=2)    #ساخت دکمه محاسه
 
-result_label.grid(row=2, column=0, columnspan=2)
-
+result_label.grid(row=2, column=0, columnspan=2)                                                                        #جانمایی لیبل نتیجه
+                                                                                                                        #جانمایی رو در آخر گذاشتیم چ.ت بعد از اعمل جانمایی نمیشه عناصر رو کانفیگ کرد
 main_form.mainloop()
